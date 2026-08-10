4105f492b9c9f8021c31b87ef31a625e89d874a6
mantelatomaycon-dot <mantelatomaycon@gmail.com>
2026-08-10 17:17:09 -0300

    Add script to display a greeting message on load

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const h1 = document.createElement("h1");
+    h1.innerText = "Hello! My script via jsDelivr is working perfectly.";
+    h1.style.color = "#007bff";
+    h1.style.fontFamily = "sans-serif";
+    h1.style.textAlign = "center";
+    h1.style.marginTop = "50px";
+    document.body.appendChild(h1);
+});
